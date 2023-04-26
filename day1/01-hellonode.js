7efcd1aabcb18ab704407005e321d5c71c0e5452
ahaoo <2281300996@qq.com>
2023-04-26 21:41:13 +0800

    init hello nodejs

---
+++ day1/01-hellonode.js
@@ -0,0 +1,17 @@
+// nodejs 基于谷歌 chromeV8 引擎 是 js 的运行环境 内置 node API 路径 网络等
+// ECMAScript 核心语法均可以使用
+let name = '蔡徐坤'
+console.log(name);
+
+function fn(){
+  let obj = {
+    name:'c0deMak1r',
+    gender:'男'
+  }
+  console.log(obj);
+}
+fn()
+
+for(let i = 1;i<6;i++){
+  console.log('蔡徐坤真爱粉！');
+}
